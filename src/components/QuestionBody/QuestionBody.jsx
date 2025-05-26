0bc9557e39753eac10c68d4641aba118103ea750
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-26 14:01:48 +0200

    handover iconType & set as answer

--- src/components/QuestionBody/QuestionBody.jsx
+++ src/components/QuestionBody/QuestionBody.jsx
@@ -1,8 +1,6 @@
 import { Icon } from '../Icon/Icon';
 import './style.css';
 
-// 3. Zařiďte následující: pokud je ve stavu `answered` hodnota `false`, chceme, aby komponenta `QuestionBody` měla ikonku `symbolQuestion`. Pokud je stav `true`, chceme, aby typ ikonky byl `symbolTick`.
-
 export const QuestionBody = ({ iconType, text }) => {
   return (
     <div className="question__body">
